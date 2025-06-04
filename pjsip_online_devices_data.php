f1311bc7a99f7c841046e9f66b59e362950f4894
pedjas <pedjas@users.noreply.github.com>
2025-06-04 19:43:08 +0200

    Script now can show all endpoints, not only those that are online

--- pjsip_online_devices_data.php
+++ pjsip_online_devices_data.php
@@ -5,30 +5,59 @@ Author: Steve Hillin <stevehillin@gmail.com>
 Disclaimer: The data displayed by this file could be 
 considered a security risk.  It is imparative that you 
 secure this page from public consumption.
+----
+
+Updated by Predrag Supurovic <pedja@supurovic.net> to
+allow listing all extensions regardles if online or not
+
 **********************************************************/
-$data = array();
+
+define('SHOW_ALL', true); // set to false to show only online extensions
+
+$cmd2 = "asterisk -rx 'database show AMPUSER'";
+$resultsAMPUSER = shell_exec($cmd2);
+$resultsAMPUSERClean = str_replace ('/AMPUSER/', '', $resultsAMPUSER);
+
+$array2 = explode(PHP_EOL, $resultsAMPUSERClean);
+
+$extensions = array();
+foreach ($array2 as $item => $value) {
+  $itemarray = explode('/', $value);
+  if (! empty($itemarray[0]) and is_numeric ($itemarray[0])) {
+    if (! isset ($extensions[$itemarray[0]]['extension']['number'])) {
+      $extensions[$itemarray[0]]['extension']['number'] = $itemarray[0];
+    }
+    $arrayvalue = explode(':', $itemarray[1]);
+
+    $extensions[$itemarray[0]]['extension'][trim($arrayvalue[0])] = $arrayvalue[1];
+    
+    //$extensions[$itemarray[0]]['online']['ip'] = '';
+    //$extensions[$itemarray[0]]['online']['user_agent'] = '';
+    //$extensions[$itemarray[0]]['online']['expiration_time'] = '';
+
+  }
+
+}
+
+
+
 $cmd = "asterisk -rx 'database show registrar/contact' | grep -v 'results'";
 $results = shell_exec($cmd);
+
 $array = explode(PHP_EOL, $results);
-foreach($array as $a)
-{
+foreach($array as $a) {
   preg_match('#\{(.*?)\}#', $a, $match);
   $json = json_decode($match[0], true);
-  $data[] = array(
-    "endpoint" => $json['endpoint'],
-    "ip" => $json['via_addr'],
-    "user_agent" => $json['user_agent'],
-    "expiration_time" => date("Y-m-d H:i:s", $json['expiration_time'])
-    );
-}
-// Sanitize
-foreach($data as $k => $v)
-{
-  if($v['endpoint'] == "")
-  {
-    unset($data[$k]);
+
+  if (! empty ($json['endpoint'])) {
+    $extensions[$json['endpoint']]['online'] = $json;
+    $extensions[$json['endpoint']]['online']['expiration_time'] = date("Y-m-d H:i:s", $json['expiration_time']);
   }
+  
 }
+
+
+
 ?>
 <table class="table table-bordered">
 <thead>
@@ -40,20 +69,30 @@ foreach($data as $k => $v)
 </tr>
 </thead>
 <tbody>
+
 <?php
-foreach($data as $k => $v)
-{
-  echo "<tr><td>";
-  echo $v['endpoint'];
-  echo "</td><td>";
-  echo $v['ip'];
-  echo "</td><td>";
-  echo $v['user_agent'];
-  echo "</td><td>";
-  echo $v['expiration_time'];
-  echo "</td></tr>";
+foreach($extensions as $k => $v) {
+  if (SHOW_ALL or (!SHOW_ALL and isset ($v['online']))) {
+?>
+<tr>
+<td>
+<?php echo $k; ?>
+</td>
+<td>
+<?php echo (isset ($v['online']) ? $v['online']['via_addr'] : ''); ?>
+</td>
+<td>
+<?php echo (isset ($v['online']) ? $v['online']['user_agent'] : ''); ?>
+</td>
+<td>
+<?php echo (isset ($v['online']) ? $v['online']['expiration_time'] : ''); ?>
+</td>
+</tr>
+<?php
+  }
 }
 ?>
+
+
 </tbody>
 </table>
-
